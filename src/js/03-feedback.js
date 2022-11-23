f52a68055223d6c6140f79fafb429bf41eaa9199
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2022-11-23 03:03:53 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,21 @@
 import throttle from 'lodash.throttle';
-const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
-
+const STORAGE_KEY = 'feedback-form-state';
 const savedSettings = localStorage.getItem(STORAGE_KEY);
 const parsedSettings = JSON.parse(savedSettings);
 
 const formData = { ...parsedSettings };
+
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onInputValue, 500));
+
 populateForm();
 
 function onFormSubmit(e) {
   e.preventDefault();
 
-  console.log(parsedSettings);
+  console.log(formData);
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
@@ -25,10 +26,10 @@ function onInputValue(e) {
 }
 
 function populateForm() {
-  if (parsedSettings.message) {
-    form.elements.message.value = parsedSettings.message;
+  if (formData.message) {
+    form.elements.message.value = formData.message;
   }
-  if (parsedSettings.email) {
-    form.elements.email.value = parsedSettings.email;
+  if (formData.email) {
+    form.elements.email.value = formData.email;
   }
 }
